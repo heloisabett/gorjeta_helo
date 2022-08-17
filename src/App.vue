ff30c286aee9545115347b5576382f7e7db0601e
heloisabett <heloisabettramosifc@gmail.com>
2022-08-17 15:01:17 -0300

    170822

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+<h2>Calculadora de Gorjeta</h2>
+<p>Qual o valor da conta?</p>
+<input type="text" />
+<p>Como foi o serviço?</p>
+<input type="text" />
+<p>Quantas pessoas estão pagando a conta?</p>
+<input type="text" />
+</template>
+<style></style>
