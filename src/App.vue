d7a9837375f75340dc71f7dd05d588bbbe396dc8
heloisabett <heloisabettramosifc@gmail.com>
2022-08-23 09:16:08 -0300

    230822

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
+<script setup>
+import { ref } from "vue";
+
+const valor2 = ref("");
+const valor1 = ref("");
+</script>
 <template>
-<h2>Calculadora de Gorjeta</h2>
-<p>Qual o valor da conta?</p>
-<input type="text" />
-<p>Como foi o serviço?</p>
-<input type="text" />
-<p>Quantas pessoas estão pagando a conta?</p>
-<input type="text" />
+  <h2>Calculadora de Gorjetas</h2>
+  <h3>Qual o valor da conta?</h3>
+  <input type="number" v-model="valor1" placeholder="" />
+   <h3>Como foi o serviço?</h3>
+  <input type="number" placeholder="" v-model="valor2" />
+  <select name="Qualidade de serviço" id="asd" v-model="gorgeta">
+    <option value="5">30%</option>
+    <option value="10">25%</option>
+    <option value="15">20%</option>
+    <option value="20">15%</option>
+    <option value="25">10%</option>
+    <option value="25">5%</option>
+  </select>
+
+  <p>Valor para cada um pagar: {{ (valor1 * (1 + gorgeta / 100)) / valor2 }}</p>
 </template>
-<style></style>
