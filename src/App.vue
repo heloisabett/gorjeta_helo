5a83c697bef4cddf95cab22e51fc1cd5db40f5ae
heloisabett <heloisabettramosifc@gmail.com>
2022-08-24 15:10:21 -0300

    240822

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,64 @@
 <script setup>
 import { ref } from "vue";
-
-const valor2 = ref("");
 const valor1 = ref("");
+const valor2 = ref("");
 </script>
 <template>
-  <h2>Calculadora de Gorjetas</h2>
-  <h3>Qual o valor da conta?</h3>
-  <input type="number" v-model="valor1" placeholder="" />
-   <h3>Como foi o serviço?</h3>
-  <input type="number" placeholder="" v-model="valor2" />
-  <select name="Qualidade de serviço" id="asd" v-model="gorgeta">
-    <option value="5">30%</option>
-    <option value="10">25%</option>
-    <option value="15">20%</option>
-    <option value="20">15%</option>
-    <option value="25">10%</option>
-    <option value="25">5%</option>
-  </select>
+  <main>
+    <h2>Calculadora de Gorjetas</h2>
 
-  <p>Valor para cada um pagar: {{ (valor1 * (1 + gorgeta / 100)) / valor2 }}</p>
+    <label>Qual o valor da conta?</label>
+    <input type="number" v-model="valor2" />
+    <label>Como foi o Serviço?</label>
+    <select name="Qualidade do serviço" v-model="gorjeta" id="asd">
+      <option value="5">5% (Péssimo)</option>
+      <option value="10">10% (Ruim)</option>
+      <option value="15">15% (Ok)</option>
+      <option value="20">20% (Bom)</option>
+      <option value="25">25% (Mais que Bom)</option>
+      <option value="30">30% (Perfeito)</option>
+    </select>
+    <label>Quantas pessoas estão pagando a conta?</label>
+    <input type="number" v-model="valor1" />
+    <h4>
+      Valor da conta: R${{ (preco * (1 + gorjeta / 100)) / pessoas }} para cada
+      pessoa
+    </h4>
+  </main>
 </template>
+<style>
+input {
+  display: block;
+  width: 250px;
+  height: 25px;
+  margin-bottom: 15px;
+  border: 1px solid rgb(255, 255, 255);
+  border-radius: 10px;
+}
+h2 {
+  background-color: rgb(171, 194, 180);
+  height: 40px;
+  padding: 3px;
+  color: rgb(255, 255, 255);
+  border-radius: 10px;
+  text-align: center;
+}
+select {
+  display: block;
+  width: 250px;
+  height: 25px;
+  margin-bottom: 15px;
+  border: 1px solid black;
+  border-radius: 10px;
+}
+
+main {
+  background-color: rgb(209, 209, 209);
+  padding: 30px;
+  margin: 50% auto;
+  border-radius: 8px;
+  margin-left: 45%;
+  height: 300px;
+  width: 500px;
+}
+</style>
